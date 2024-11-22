639bd26d0ff0a522874e54b1e5ea070217c42ace
athanase25537 <andriamasyathanase.nazy@gmail.com>
2024-11-22 13:27:10 +0300

    add chat front

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template, redirect, url_for
 
 app = Flask(__name__)
 
 @app.route("/home")
 def home():
-    return "Hello World"
+    return render_template("index.html")
 
+
+@app.route("/admin")
+def admin():
+    return redirect(url_for("home", name='Admin'))
 if __name__ == "__main__":
     app.run()
